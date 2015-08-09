4b389296209fe0970869b20bb96cd77d6cd483ab
Jim Owens <jim@26maidenlane.net>
2015-08-09 13:29:22 -0400

    Fixed presence_probe handler

--- chatagent3.py
+++ chatagent3.py
@@ -29,22 +29,29 @@ from time import strftime
 
 class ChatAgent(sleekxmpp.ClientXMPP):
     '''Class defining semi-autonomous XMPP chat agents'''
+    # These variables are used to pace the activity
+    # rate of the agents and the probability that
+    # they'll send a message when their turn comes up
     delay_min = 30 # seconds
     delay_max = 60 # seconds
     send_prob = 0.1
-    # A list to which agents that crash will be added
-    # so that they can later be respawned by the driver script
-    ###crashed = []
+
     def __init__(self, jid, password):
         'Create a chat agent'
         sleekxmpp.ClientXMPP.__init__(self, jid, password)
+        # Using these seetings, we don't need to handle
+        # subscription requests
         self.auto_authorize = True
         self.auto_subscribe = True
+        # Save the account name for printing
+        # in various messages
         self.__whoami = jid
         self.__sched = sched.scheduler(time.time, time.sleep)
         self.add_event_handler('session_start', self._start)
         self.add_event_handler('presence_probe', self._handle_probe)
-        self.add_event_handler('presence_subscribe', self._handle_subscribe)
+        # Probably don't need the subscribe handler with
+        # options for auto_authorize and auto_subscribe set above
+        #self.add_event_handler('presence_subscribe', self._subscribe)
         self.add_event_handler('message', self._message)
         self.register_plugin('xep_0030')
         self.register_plugin('xep_0004')
@@ -71,8 +78,7 @@ class ChatAgent(sleekxmpp.ClientXMPP):
         self.get_roster()
         
     def _handle_probe(self, presence):
-        sender = presence['from']
-        self.sendPresence(pto=sender, pstatus="Let's chat!", pshow="available")
+        self.sendPresence(pto = event["from"])
     
     def _handle_subscribe(self, presence):
         sender = presence['from']
@@ -82,10 +88,10 @@ class ChatAgent(sleekxmpp.ClientXMPP):
 
     def _message(self, msg):
         if msg['type'] in ('chat', 'normal'):
-            print(strftime("%H:%M:%S") + ' ' + self.__whoami + 'received a message from', msg['from'])
+            print(strftime("%H:%M:%S") + ' ' + self.__whoami + ' received a message from', str(msg['from']).split('/')[0])
                 
     def _sched_msg(self):
-        'Schedule a message to be sent after a random delay'
+        'Schedule a message to be sent after a random delay, but only if we have an empty queue'
         if self.__sched.empty():
             # Scheduling a message is based on the
             # probability defined as a class variable
@@ -105,79 +111,40 @@ class ChatAgent(sleekxmpp.ClientXMPP):
             f.close()
         if not to:
             while True:
-                # Pick a random jid from our roster
+                # Pick a random jid from our roster, but first
+                # check whether the roster actually contains any
+                # chat buddies at this point...
                 if len(list(self.client_roster.keys())) < 1:
                     print(self.__whoami, 'has NO friends!')
                     print(strftime("%H:%M:%S") + ' ' + self.__whoami, 'has NO friends!')
                     return
                 to = random.choice(list(self.client_roster.keys()))
-                # Get my (stripped) jid
-                #me = self.__my_jid.getNode() + '@' + self.__my_jid.getDomain()
                 me = self.__whoami
-                # Let's not try to send messages to myself
+                # Let's not send messages to myself, so we'll loop
+                # until the randomly-selected addressee isn't me. This
+                # was an issue with the Python 2 version of xmpp, but
+                # may not be with sleekxmpp. Still, it doesn't hurt
+                # to check.
                 if to != me:
                     break
-        # Is the recipient online?
-        #if self._roster.getShow(to) != None:
         print(strftime("%H:%M:%S") + ' ' + self.__whoami + ': sending message to:', to)
-            #message = xmpp.Message(to, msg)
-            #message.setAttr('type', 'chat')
-            #self.__client.send(message)
         self.send_message(to, msg)
-        #else:
-        #print(strftime("%H:%M:%S") + ' ' + self.__whoami + ':', to, 'is not on line')
-            #self._roster.Subscribe(to)
-            #self._roster.Authorize(to)
 
-    def _message(self, sess, mess):
-        'Method to handle receipt of XMPP messages'
-        mess_text = mess.getBody()
-        mess_from = mess.getFrom().getStripped()
-        print(strftime("%H:%M:%S") + ' ' + self.__whoami, 'received message from: ', mess_from)
-        # Sending a reply is based on the probability
-        # of doing so, defined as a class variable
-        r = random.randint(1,101)
-        p = int(1 / ChatAgent.send_prob)
-        if r % p == 0:
-            f = os.popen('fortune')
-            reply = f.read().strip()
-            f.close()
-            mess_reply = mess.buildReply(reply)
-            self.send_msg(mess_reply, mess_from)
+#    This is the old, Python 2 version, replaced by a simpler version that
+#    simply prints the fact of a message's receipt
+#    def _message(self, sess, mess):
+#        'Method to handle receipt of XMPP messages'
+#        mess_text = mess.getBody()
+#        mess_from = mess.getFrom().getStripped()
+#        print(strftime("%H:%M:%S") + ' ' + self.__whoami, 'received message from: ', mess_from)
+#        # Sending a reply is based on the probability
+#        # of doing so, defined as a class variable
+#        r = random.randint(1,101)
+#        p = int(1 / ChatAgent.send_prob)
+#        if r % p == 0:
+#            f = os.popen('fortune')
+#            reply = 'Thanks for checking in!\n' +  f.read().strip()
+#            f.close()
+#            mess_reply = mess.buildReply(reply)
+#            self.send_msg(mess_reply, mess_from)
 
-#    def _presence(self,conn,msg):
-#        'Method to handle XMPP presence'
-#        jid=xmpp.protocol.JID(msg.getFrom())
-#        name=self._roster.getName(jid.getNode()+"@"+jid.getDomain())
-#        if not name:
-#            name=jid.getNode()+"@"+jid.getDomain()
-#        if not self.__showself and name == self.__my_jid.getNode()+"@"+self.__my_jid.getDomain():
-#            return
-#        status = msg.getStatus()
-#        show = msg.getShow()
-#        if not msg.getPriority():
-#            status="Signed out"
-#        elif not status and show:
-#            status="("+msg.getShow()+")"
-#        elif show and status:
-#            status="("+msg.getShow()+") " + status
-#        for f in self.__callbacks:
-#            f(name,status)
-        
-#class ConnectionException(Exception):
-#    def __init__(self):
-#        pass
-#    def __str__(self):
-#        return "Error connecting to chat server"
-#
-#class AuthException(Exception):
-#    def __init__(self):
-#        pass
-#    def __str__(self):
-#        print "Error authenticating to chat server"
-#
-#if __name__=="__main__":
-#    def callback(name,status):
-#        print "%s: %s" % (name,status)
-#        
-#    DistTwit(sys.argv[1],sys.argv[2],callbacks=[callback])
